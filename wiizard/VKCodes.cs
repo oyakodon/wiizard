3f51ce381a5b6143686c0c326d63f660a0e469e0
Oyakodon <oykdn@oykdn.com>
2018-03-03 12:42:42 +0900

    :+1: Json.NETを導入し、VKCodesを実装

---
+++ wiizard/VKCodes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace wiizard
+{
+    public class VKCodes
+    {
+        public VKCodes()
+        {
+            // load json file
+            string json = null;
+            using (var sr = new StreamReader(@"./VKCodes.json", System.Text.Encoding.UTF8))
+            {
+                json = sr.ReadToEnd();
+            }
+
+            m_vkcodes = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        }
+
+        public byte this[string code]
+        {
+            get { return Convert.ToByte(m_vkcodes[code], 16); }
+        }
+
+        private Dictionary<string, string> m_vkcodes;
+
+    }
+}
