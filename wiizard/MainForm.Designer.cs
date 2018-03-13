73ae2671cd45c57024cc327e9436d1ca06c82ddb
Oyakodon <oykdn@oykdn.com>
2018-03-13 23:59:10 +0900

    :construction: 設定(編集)UIの実装

--- wiizard/MainForm.Designer.cs
+++ wiizard/MainForm.Designer.cs
@@ -42,8 +42,12 @@
             this.panel_profile = new System.Windows.Forms.Panel();
             this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.listBox_profile = new System.Windows.Forms.ListBox();
-            this.btnRun = new System.Windows.Forms.Button();
             this.picBox_wii = new System.Windows.Forms.PictureBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tabControl_config = new System.Windows.Forms.TabControl();
+            this.tabPage_buttons = new System.Windows.Forms.TabPage();
+            this.tabPage_AccIR = new System.Windows.Forms.TabPage();
+            this.panel_config_wiiimg = new System.Windows.Forms.Panel();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.panel_profile.SuspendLayout();
@@ -52,6 +56,9 @@
             this.splitContainer.Panel2.SuspendLayout();
             this.splitContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picBox_wii)).BeginInit();
+            this.tabControl_config.SuspendLayout();
+            this.tabPage_buttons.SuspendLayout();
+            this.panel_config_wiiimg.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -159,7 +166,7 @@
             // splitContainer.Panel2
             // 
             this.splitContainer.Panel2.BackColor = System.Drawing.SystemColors.Control;
-            this.splitContainer.Panel2.Controls.Add(this.picBox_wii);
+            this.splitContainer.Panel2.Controls.Add(this.tabControl_config);
             this.splitContainer.Panel2.Controls.Add(this.btnRun);
             this.splitContainer.Size = new System.Drawing.Size(929, 536);
             this.splitContainer.SplitterDistance = 196;
@@ -205,6 +212,17 @@
             this.listBox_profile.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox_profile_DrawItem);
             this.listBox_profile.SelectedIndexChanged += new System.EventHandler(this.listBox_profile_SelectedIndexChanged);
             // 
+            // picBox_wii
+            // 
+            this.picBox_wii.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox_wii.Image = ((System.Drawing.Image)(resources.GetObject("picBox_wii.Image")));
+            this.picBox_wii.Location = new System.Drawing.Point(10, 10);
+            this.picBox_wii.Name = "picBox_wii";
+            this.picBox_wii.Size = new System.Drawing.Size(695, 392);
+            this.picBox_wii.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox_wii.TabIndex = 1;
+            this.picBox_wii.TabStop = false;
+            // 
             // btnRun
             // 
             this.btnRun.Location = new System.Drawing.Point(156, 465);
@@ -215,15 +233,47 @@
             this.btnRun.UseVisualStyleBackColor = true;
             this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
             // 
-            // picBox_wii
+            // tabControl_config
             // 
-            this.picBox_wii.Image = ((System.Drawing.Image)(resources.GetObject("picBox_wii.Image")));
-            this.picBox_wii.Location = new System.Drawing.Point(34, 17);
-            this.picBox_wii.Name = "picBox_wii";
-            this.picBox_wii.Size = new System.Drawing.Size(670, 433);
-            this.picBox_wii.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picBox_wii.TabIndex = 1;
-            this.picBox_wii.TabStop = false;
+            this.tabControl_config.Controls.Add(this.tabPage_buttons);
+            this.tabControl_config.Controls.Add(this.tabPage_AccIR);
+            this.tabControl_config.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl_config.Location = new System.Drawing.Point(0, 0);
+            this.tabControl_config.Name = "tabControl_config";
+            this.tabControl_config.SelectedIndex = 0;
+            this.tabControl_config.Size = new System.Drawing.Size(729, 444);
+            this.tabControl_config.TabIndex = 2;
+            // 
+            // tabPage_buttons
+            // 
+            this.tabPage_buttons.Controls.Add(this.panel_config_wiiimg);
+            this.tabPage_buttons.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_buttons.Name = "tabPage_buttons";
+            this.tabPage_buttons.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_buttons.Size = new System.Drawing.Size(721, 418);
+            this.tabPage_buttons.TabIndex = 0;
+            this.tabPage_buttons.Text = "ボタン";
+            this.tabPage_buttons.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_AccIR
+            // 
+            this.tabPage_AccIR.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_AccIR.Name = "tabPage_AccIR";
+            this.tabPage_AccIR.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_AccIR.Size = new System.Drawing.Size(721, 418);
+            this.tabPage_AccIR.TabIndex = 1;
+            this.tabPage_AccIR.Text = "加速度・IR";
+            this.tabPage_AccIR.UseVisualStyleBackColor = true;
+            // 
+            // panel_config_wiiimg
+            // 
+            this.panel_config_wiiimg.Controls.Add(this.picBox_wii);
+            this.panel_config_wiiimg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_config_wiiimg.Location = new System.Drawing.Point(3, 3);
+            this.panel_config_wiiimg.Name = "panel_config_wiiimg";
+            this.panel_config_wiiimg.Padding = new System.Windows.Forms.Padding(10);
+            this.panel_config_wiiimg.Size = new System.Drawing.Size(715, 412);
+            this.panel_config_wiiimg.TabIndex = 2;
             // 
             // MainForm
             // 
@@ -249,6 +299,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
             this.splitContainer.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.picBox_wii)).EndInit();
+            this.tabControl_config.ResumeLayout(false);
+            this.tabPage_buttons.ResumeLayout(false);
+            this.panel_config_wiiimg.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -271,6 +324,10 @@
         private System.Windows.Forms.ToolStripMenuItem MenuItem_autoReload;
         private System.Windows.Forms.ToolStripMenuItem MenuItem_debugInfo;
         private System.Windows.Forms.PictureBox picBox_wii;
+        private System.Windows.Forms.TabControl tabControl_config;
+        private System.Windows.Forms.TabPage tabPage_buttons;
+        private System.Windows.Forms.TabPage tabPage_AccIR;
+        private System.Windows.Forms.Panel panel_config_wiiimg;
     }
 }
 
